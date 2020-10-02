82e5d1baf89453e66b968d41bb11c3e25a776eef
Artoym <44746890+Flashbuck@users.noreply.github.com>
2020-10-02 09:33:47 +0300

    feat: add script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+exports.min = function min (array = 0) {
+  if(array == 0){
+    return 0;
+  }
+  let min = 0;
+  for (let i = 0; i < array.length; i++ ) array[i] < min ? min = array[i] : false;
+  return min;
+}
 
+exports.max = function max (array = 0) {
+  if(array == 0){
+    return 0;
+  } 
+  let max = 0;
+  for (let i = 0; i < array.length; i++ ) array[i] > max ? max = array[i] : false;
+  return max;
+}
+
+exports.avg = function avg (array = 0) {
+  if(array == 0){
+    return 0;
+  } 
+  let sum = 0;
+  for (let i = 0; i < array.length; i++ ) sum += array[i];
+  return sum == 0 ? sum : sum / array.length;
+}
